491d1df0a43cd2258e8d8b8918141740070841dc
Marcelo Fortunato <mp.fortunato88@gmail.com>
2024-11-15 19:28:02 +0000

    feat: Setup sentry o11y

--- packages/next-config/instrumentation.ts
+++ packages/next-config/instrumentation.ts
@@ -1,16 +1,49 @@
 import { env } from '@repo/env';
 import { init } from '@sentry/nextjs';
 
-const opts = {
-  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+// const opts = {
+//   dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+// };
+
+const optsServer = {
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.NODE_ENV !== 'test',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development'
+};
+
+const optsEdge = {
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled: process.env.NODE_ENV !== 'test',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 };
 
 export const initializeSentry = () => {
   if (env.NEXT_RUNTIME === 'nodejs') {
-    init(opts);
+    init(optsServer);
   }
 
   if (env.NEXT_RUNTIME === 'edge') {
-    init(opts);
+    init(optsEdge);
   }
+
+  // if (process.env.NEXT_RUNTIME === 'nodejs') {
+  //   await import('./sentry.server.config');
+  // }
+
+  // if (process.env.NEXT_RUNTIME === 'edge') {
+  //   await import('./sentry.edge.config');
+  // }
 };
